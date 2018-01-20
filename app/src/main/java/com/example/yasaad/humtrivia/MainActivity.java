470322103827bdfd8de2be899bb694a6c77be533
yasaad <youssef.a.asaad@gmail.com>
2018-01-20 13:25:33 -0500

    Sign in

--- app/src/main/java/com/example/yasaad/humtrivia/MainActivity.java
+++ app/src/main/java/com/example/yasaad/humtrivia/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.yasaad.humtrivia;
 
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
@@ -14,6 +15,7 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.yasaad.humtrivia.UI.LoginActivity;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
@@ -109,6 +111,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
         if (view == textViewSignin) {
             // open login activity here
+            startActivity(new Intent(MainActivity.this, LoginActivity.class));
         }
     }
     @Override
@@ -141,7 +144,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         if (currentUser != null) {
             Toast.makeText(MainActivity.this, currentUser.getEmail()
                     , Toast.LENGTH_SHORT).show();
-            //startActivity(new Intent(MainActivity.this, HomeScreen.class));
         } else {
             Toast.makeText(MainActivity.this, "No user signed in", Toast.LENGTH_SHORT)
                     .show();
